c50ba318861b72b60ab2fdc694dbbf65f73daa26
Vitor Buxbaum <vitor.buxbaum@betrybe.com>
2021-02-09 18:20:56 -0300

    Requisito 3, adequado a todos os testes

--- tech_news/collector/importer.py
+++ tech_news/collector/importer.py
@@ -1,2 +1,24 @@
+import csv
+
+
 def csv_importer(filepath):
-    """Seu código deve vir aqui"""
+
+    if not filepath.endswith(".csv"):
+        raise ValueError("Formato invalido")
+
+    import_result = []
+    try:
+        with open(filepath, newline="") as csvfile:
+            for row in csv.DictReader(csvfile, delimiter=";"):
+                import_result.append(build_dict_from_csv_row(row))
+    except IOError:
+        raise ValueError(f"Arquivo {filepath} não encontrado")
+    else:
+        return import_result
+
+
+def build_dict_from_csv_row(row):
+    built_dict = {}
+    for key, value in row.items():
+        built_dict[key] = value
+    return built_dict
